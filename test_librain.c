82a715455f247a19a4b6daa255b4b3a555a5a777
jfsmig <jfsmig@localhost>
2014-06-27 22:39:02 +0200

    Test code factorized

--- test_librain.c
+++ test_librain.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <time.h>
-#include <assert.h>
 #include <alloca.h>
 
 #include "./librain.h"
-#include "./utils.h"
+#include "./test_utils.h"
 
-#define kiB 1024
-#define MiB (kiB*kiB)
-
-#define BUFFSIZE 512
+static inline unsigned int
+_count_positives (int *tab)
+{
+	unsigned int count = 0;
+	for (; tab[count]>= 0 ;++count) {}
+	return count;
+}
 
 static const char *
 _algo_to_str (enum rain_algorithm_e algo)
@@ -23,63 +23,19 @@ _algo_to_str (enum rain_algorithm_e algo)
 	}
 }
 
-static size_t
-_elapsed_usec (struct timespec pre, struct timespec post)
-{
-	struct timespec diff;
-
-	if (post.tv_nsec < pre.tv_nsec) {
-		diff.tv_sec = post.tv_sec - pre.tv_sec - 1;
-		diff.tv_nsec = (1000000000 + post.tv_nsec) - pre.tv_nsec;
-	} else {
-		diff.tv_sec = post.tv_sec - pre.tv_sec;
-		diff.tv_nsec = post.tv_nsec - pre.tv_nsec;
-	}
-	return ((1000000000 * diff.tv_sec) + diff.tv_nsec) / 1000;
-}
-
-static void
-randomize(uint8_t *b, size_t s)
-{
-	uint8_t tmp[BUFFSIZE];
-
-	memset(b, 0, s);
-
-	// Fill a buffer of random bytes
-	FILE *f = fopen("/dev/urandom", "r");
-	for (size_t total=0; total < BUFFSIZE ;) {
-		ssize_t r = fread(tmp+total, 1, BUFFSIZE-total, f);
-		assert (r >= 0);
-		total += r;
-	}
-	fclose(f);
-	f = NULL;
-	
-	// Repeat the buffer into the target
-	size_t low = _lower_multiple(s, BUFFSIZE);
-	if (low) {
-		for (size_t total=0; total < low ;total+=BUFFSIZE)
-			memcpy(b+total, tmp, BUFFSIZE);
-	}
-	if (low != s)
-		memcpy(b+low, tmp, s-low);
-}
-
 static void
 test_size (size_t length, const char *algo, unsigned int k, unsigned int m)
 {
 	struct rain_encoding_s enc;
 
 	if (rain_get_encoding (&enc, length, k, m, algo)) {
-
 		double overhead = (double)((k+m)*enc.block_size) / (double) enc.data_size;
-
-		printf("SIZE %s %u+%u W=%u PS=%lu s=%lu DS=%lu PDS=%lu BS=%lu %f\n",
+		PRINTF ("SIZE %s %u+%u W=%u PS=%lu s=%lu DS=%lu PDS=%lu BS=%lu %f\n",
 				algo, k, m , enc.w, enc.packet_size, enc.strip_size,
 				enc.data_size, enc.padded_data_size, enc.block_size,
 				overhead);
 	} else {
-		printf("SIZE %s %u+%u W=%u PS=%lu s=%lu DS=%lu PDS=%lu BS=%lu -\n",
+		PRINTF ("SIZE %s %u+%u W=%u PS=%lu s=%lu DS=%lu PDS=%lu BS=%lu -\n",
 				algo, k, m , enc.w, enc.packet_size, enc.strip_size,
 				enc.data_size, enc.padded_data_size, enc.block_size);
 	}
@@ -104,54 +60,42 @@ test_encoding (size_t length, const char *algo, unsigned int k, unsigned int m)
 	int rc = 0;
 
 	if (rain_get_encoding (&enc, length, k, m, algo)) {
-		uint8_t *buf, *parity[m+1];
+		uint8_t *buf, *parity[m];
 
-		posix_memalign((void**)&buf, sizeof(long), enc.padded_data_size);
-		randomize(buf, length);
-		memset(buf + length, 0, enc.padded_data_size - length);
-		memset(parity, 0, (m+1) * sizeof(uint8_t*));
+		rc = posix_memalign ((void**)&buf, sizeof(long), enc.padded_data_size);
+		assert(rc == 0 && buf != NULL);
+		randomize (buf, length);
+		memset (buf + length, 0, enc.padded_data_size - length);
+		memset (parity, 0, m * sizeof(uint8_t*));
 
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &pre);
+		clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &pre);
 		rc = rain_encode (buf, length, &enc, NULL, parity);
-		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &post);
+		clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &post);
 
-		free(buf);
-		assert(rc != 0);
+		free (buf);
+		assert (rc != 0);
 		for (unsigned int i=0; i<m ;++i) {
-			assert(parity[i] != NULL);
-			free(parity[i]);
+			assert (parity[i] != NULL);
+			free (parity[i]);
 		}
-		assert(parity[m] == NULL);
 	}
 
 	if (!rc) {
-		printf("ENC %s %u+%u DS=%lu - -\n",
-				algo, k, m, enc.data_size);
+		PRINTF("ENC %s %u+%u DS=%lu - -\n", algo, k, m, enc.data_size);
 	} else {
-
-		size_t elapsed = _elapsed_usec(pre, post);
-		double throughput = ((double) enc.data_size / (double) elapsed) * 1000000.0;
-		static const double DGiB = 1.0 * (1024 * MiB);
-
-		printf("ENC %s %u+%u DS=%lu %lu %f\n",
-				algo, k, m, enc.data_size, elapsed, throughput / DGiB);
+		size_t elapsed = _elapsed_msec (pre, post);
+		double throughput = ((double) enc.data_size / (double) elapsed) * 1000.0;
+		PRINTF("ENC %s %u+%u DS=%lu %lu %f\n",
+				algo, k, m, enc.data_size, elapsed, throughput / (double)GiB);
 	}
-}
-
-static unsigned int
-_count_positives (int *tab)
-{
-	unsigned int count = 0;
-	for (; tab[count]>= 0 ;++count) {}
-	return count;
 }
 
 static void
 test_rehydrate (struct rain_encoding_s *enc, uint8_t **data, uint8_t **parity,
 		int *deleted)
 {
-	unsigned int count_deleted = _count_positives(deleted);
-	assert(count_deleted < enc->k + enc->m);
+	unsigned int count_deleted = _count_positives (deleted);
+	assert (count_deleted < enc->k + enc->m);
 
 	// save the blocs to be restored
 	uint8_t *saved[count_deleted];
@@ -159,45 +103,43 @@ test_rehydrate (struct rain_encoding_s *enc, uint8_t **data, uint8_t **parity,
 		if (i < enc->k) {
 			saved[i] = data[i];
 			data[i] = NULL;
-		}
-		else {
+		} else {
 			saved[i] = parity[i - enc->k];
 			parity[i - enc->k] = NULL;
 		}
 	}
 
-	fprintf(stderr, "REHYDRATE %s %u %u lost=%u %lu", _algo_to_str(enc->algo),
+	PRINTF ("REHYDRATE %s %u %u lost=%u %lu", _algo_to_str(enc->algo),
 			enc->k, enc->m, count_deleted, enc->data_size);
 
 	struct timespec pre, post;
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &pre);
+	clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &pre);
 	int rc = rain_rehydrate (data, parity, enc, NULL);
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &post);
+	clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &post);
 	assert(rc != 0);
 
-	size_t elapsed = _elapsed_usec(pre, post);
-	double throughput = ((double)enc->data_size) / (double)elapsed;
-	throughput *= 1000000.0;
-	fprintf(stderr, " %f %f\n", ((double)elapsed)/1000000.0, throughput);
+	size_t elapsed = _elapsed_msec (pre, post);
+	double throughput = (((double)enc->data_size) / (double)elapsed) * 1000.0;
+	PRINTF(" %lu %f\n", elapsed, throughput);
 
-	// Check the blocks are correct
+	// Check the restored blocks are identical to the originals
 	for (unsigned int i=0; i<count_deleted ;++i) {
 		if (i < enc->k) {
-			assert(data[i] != NULL);
-			assert(0 == memcmp(data[i], saved[i], enc->block_size));
+			assert (data[i] != NULL);
+			assert (0 == memcmp(data[i], saved[i], enc->block_size));
 		} else {
-			assert(parity[i - enc->k] != NULL);
-			assert(0 == memcmp(parity[i - enc->block_size], saved[i], enc->block_size));
+			assert (parity[i - enc->k] != NULL);
+			assert (0 == memcmp(parity[i - enc->block_size], saved[i], enc->block_size));
 		}
 	}
 
-	// restore the blocks to be restored
+	// free the restored blocks and relink the originals
 	for (unsigned int i=0; i<count_deleted ;++i) {
 		if (i < enc->k) {
-			free(data[i]);
+			free (data[i]);
 			data[i] = saved[i];
 		} else {
-			free(parity[i - enc->k]);
+			free (parity[i - enc->k]);
 			parity[i - enc->k] = saved[i];
 		}
 	}
@@ -219,15 +161,14 @@ _generator_rehydrate(struct rehydrator_s *gen,
 		if (tail <= 0 || remaining >= gen->enc->m) // nothing deleted
 			return;
 		test_rehydrate (gen->enc, gen->data, gen->parity, gen->deleted);
-	}
-	else {
+	} else {
 		// Do not delete block[tail] and recurse
-		_generator_rehydrate(gen, tail+1, remaining);
+		_generator_rehydrate (gen, tail+1, remaining);
 
 		// Delete block[tail], recurse, and undelete
 		unsigned int count = _count_positives (gen->deleted);
 		gen->deleted[count] = tail;
-		_generator_rehydrate(gen, tail+1, remaining-1);
+		_generator_rehydrate (gen, tail+1, remaining-1);
 		gen->deleted[count] = -1;
 	}
 }
@@ -245,12 +186,13 @@ test_roundtrip (size_t length, const char *algo, unsigned int k, unsigned int m)
 
 	// Prepare a padded buffer
 	uint8_t *buf = NULL;
-	posix_memalign((void**)&buf, sizeof(long), enc.padded_data_size);
-	randomize(buf, length);
-	memset(buf + length, 0, enc.padded_data_size - length);
+	rc = posix_memalign ((void**)&buf, sizeof(long), enc.padded_data_size);
+	assert(rc == 0 && buf != NULL);
+	randomize (buf, length);
+	memset (buf + length, 0, enc.padded_data_size - length);
 
 	// Prepare the recursion context
-	memset(&gen, 0, sizeof(gen));
+	memset (&gen, 0, sizeof(gen));
 	gen.enc = &enc;
 	gen.data = alloca(k * sizeof(uint8_t*));
 	gen.parity = alloca(m * sizeof(uint8_t*));
@@ -265,23 +207,19 @@ test_roundtrip (size_t length, const char *algo, unsigned int k, unsigned int m)
 	// Encode once and try all the combinations of lost blocks
 	rc = rain_encode (buf, length, &enc, NULL, gen.parity);
 	assert(rc != 0);
-	_generator_rehydrate(&gen, 0, m);
+	_generator_rehydrate (&gen, 0, m);
 
-	free(buf);
+	free (buf);
+	for (size_t i=0; i<m ;++i) {
+		free (gen.parity[i]);
+		gen.parity[i] = NULL;
+	}
 }
 
 int
 main(int argc, char **argv)
 {
 	(void) argc, (void) argv;
-
-	// Benchmark the rehydrating throughput
-	for (size_t length = 1*kiB; length <= 512*MiB ; length*=4) {
-		for (unsigned int k=2; k<8 ;++k)
-			test_roundtrip (length, "liber8tion", k, 2);
-		for (unsigned int k=4; k<11 ;++k)
-			test_roundtrip (length, "crs", k, 4);
-	}
 
 	// Checks
 	for (size_t length = 1*kiB; length <= 1048*MiB ; length*=2) {
@@ -292,11 +230,19 @@ main(int argc, char **argv)
 	}
 
 	// Benchmark the encoding throughput
-	for (size_t length = 1*kiB; length <= 512*MiB ; length*=4) {
+	for (size_t length = 1*MiB; length <= 256*MiB ; length*=4) {
 		for (unsigned int k=2; k<8 ;++k)
 			test_encoding (length, "liber8tion", k, 2);
 		for (unsigned int k=4; k<11 ;++k)
 			test_encoding (length, "crs", k, 4);
+	}
+
+	// Benchmark the rehydrating throughput
+	for (size_t length = 8*MiB; length <= 256*MiB ; length*=8) {
+		for (unsigned int k=2; k<8 ;++k)
+			test_roundtrip (length, "liber8tion", k, 2);
+		for (unsigned int k=7; k<11 ;++k)
+			test_roundtrip (length, "crs", k, 4);
 	}
 
 	return 0;
---
+++ test_utils.h
@@ -0,0 +1,58 @@
+#ifndef LIBRAIN_test_utils_h
+# define LIBRAIN_test_utils_h 1
+
+# include <stdio.h>
+# include <time.h>
+# include <assert.h>
+
+# include "utils.h"
+
+# define PRINTF(FMT,...) fprintf(stdout, FMT, ##__VA_ARGS__)
+# define kiB 1024
+# define MiB (kiB*kiB)
+# define GiB (kiB*kiB*kiB)
+# define BUFFSIZE 1024
+
+static inline void
+randomize(uint8_t *b, size_t s)
+{
+	uint8_t tmp[BUFFSIZE];
+
+	memset(b, 0, s);
+
+	// Fill a buffer of random bytes
+	FILE *f = fopen("/dev/urandom", "r");
+	for (size_t total=0; total < BUFFSIZE ;) {
+		ssize_t r = fread(tmp+total, 1, BUFFSIZE-total, f);
+		assert (r >= 0);
+		total += r;
+	}
+	fclose(f);
+	f = NULL;
+	
+	// Repeat the buffer into the target
+	size_t low = _lower_multiple(s, BUFFSIZE);
+	if (low) {
+		for (size_t total=0; total < low ;total+=BUFFSIZE)
+			memcpy(b+total, tmp, BUFFSIZE);
+	}
+	if (low != s)
+		memcpy(b+low, tmp, s-low);
+}
+
+static inline size_t
+_elapsed_msec (struct timespec pre, struct timespec post)
+{
+	struct timespec diff;
+
+	if (post.tv_nsec < pre.tv_nsec) {
+		diff.tv_sec = post.tv_sec - pre.tv_sec - 1;
+		diff.tv_nsec = (1000000000 + post.tv_nsec) - pre.tv_nsec;
+	} else {
+		diff.tv_sec = post.tv_sec - pre.tv_sec;
+		diff.tv_nsec = post.tv_nsec - pre.tv_nsec;
+	}
+	return 1000 * diff.tv_sec + _upper_multiple (diff.tv_nsec, 1000000) / 1000000;
+}
+
+#endif // LIBRAIN_test_utils_h
