7337c6a69b5c18b310f1aa700bc2461cd428fa59
Florent VENNETIER <florent.vennetier@worldline.com>
2015-01-02 15:21:13 +0100

    Fix encoding/decoding of small contents
    The library refused to work with small contents with more than
    one chunk of padding. Now the function encoding_prepare() may
    return suboptimal configurations for these contents, but the returned
    structure is always OK for use with rain_encode() and rain_rehydrate().
    
    The packet size has been optimized, and we have less cases of
    "more than one chunk of padding".
    
    The test_librain executable is now compiled by default.
    
    Change-Id: I543cca55381713e45cbae478352ed48dd82cac6b

--- librain.c
+++ librain.c
@@ -59,21 +59,31 @@ encoding_prepare (struct rain_encoding_s *enc,
 		enc->w = 4;
 	}
 
-	for (enc->packet_size=2048; enc->packet_size>=64 ; enc->packet_size/=2) {
+	// Retry with intermediate values (this loop can be optimized)
+	for (size_t start = 2048; start >= 1280; start -= 256) {
+		for (size_t p_size = start; p_size >= 64; p_size /= 2) {
+			enc->packet_size = p_size;
+			enc->strip_size = enc->packet_size * enc->w;
+			const size_t ks = enc->k * enc->strip_size;
+			enc->padded_data_size = _upper_multiple(enc->data_size, ks);
+			enc->block_size = enc->padded_data_size / enc->k;
 
-		enc->strip_size = enc->packet_size * enc->w;
-		const size_t ks = enc->k * enc->strip_size;
-		enc->padded_data_size = _upper_multiple(enc->data_size, ks);
-		enc->block_size = enc->padded_data_size / enc->k;
-
-		// More than a block of padding ?
-		if ((enc->padded_data_size != enc->data_size)
-			&& (enc->data_size < (enc->padded_data_size - enc->block_size)))
-				continue;
-		return 1;
+			// More than a block of padding ?
+			if ((enc->padded_data_size != enc->data_size)
+				&& (enc->data_size < (enc->padded_data_size - enc->block_size)))
+					continue;
+			return 1;
+		}
 	}
 
-	return 0;
+	// We will have more than one block of padding, but it will work
+	enc->packet_size = 64;
+	enc->strip_size = enc->packet_size * enc->w;
+	const size_t ks = enc->k * enc->strip_size;
+	enc->padded_data_size = _upper_multiple(enc->data_size, ks);
+	enc->block_size = enc->padded_data_size / enc->k;
+
+	return 1;
 }
 
 /* ------------------------------------------------------------------------- */
@@ -126,7 +136,7 @@ rain_rehydrate(uint8_t **data, uint8_t **coding,
 		}
 	}
 	if (num_erased > enc->m) // so sad ... not recoverable
-		return EXIT_FAILURE;
+		return 0;
 
 	/* Now allocate data & coding for missing parts */
 	for (unsigned int i=0; i < num_erased; i++) {
@@ -136,7 +146,7 @@ rain_rehydrate(uint8_t **data, uint8_t **coding,
 			assert(data[idx] == NULL);
 			data[idx] = block;
 		} else {
-			assert(coding[idx] == NULL);
+			assert(coding[idx - enc->k] == NULL);
 			coding[idx - enc->k] = block;
 		}
 	}
@@ -164,6 +174,8 @@ rain_encode (uint8_t *rawdata, size_t rawlength,
 	assert(rawdata != NULL);
 	assert(rawlength > 0);
 
+	int padding_chunks = 0;
+
 	if (!env)
 		env = &env_DEFAULT;
 
@@ -186,21 +198,23 @@ rain_encode (uint8_t *rawdata, size_t rawlength,
 
 	// Prepare the empty parity blocks
 	uint8_t *parity [encoding->m];
-	for (unsigned int i=0; i < encoding->m ;++i) 
+	for (unsigned int i=0; i < encoding->m; ++i)
 		parity[i] = (uint8_t*) env->calloc(sizeof(uint8_t), encoding->block_size);
 
 	// Prepare the data blocks (no copy, point to the original)
 	uint8_t *data [encoding->k];
 	size_t srcoffset = 0;
-	for (unsigned int i=0; i < encoding->k ;++i) {
-		data[i] = rawdata + srcoffset;
+	unsigned int cur_chunk = 0;
+	for (; cur_chunk < encoding->k && srcoffset < rawlength; ++cur_chunk) {
+		data[cur_chunk] = rawdata + srcoffset;
 		srcoffset += encoding->block_size;
 	}
+	// Point on the last chunk with actual data
+	--cur_chunk;
 
-	// If a padding is  necessary, replace the last data block
+	// If a padding is necessary, replace the last provided data block
 	// with a padded copy (no way to check if the original copy
 	// is long enough.
-
 	uint8_t *last_with_padding = NULL;
 	size_t tail_length = rawlength % encoding->block_size;
 	if (tail_length != 0) {
@@ -209,7 +223,12 @@ rain_encode (uint8_t *rawdata, size_t rawlength,
 		assert(last_with_padding != NULL);
 
 		memcpy(last_with_padding, rawdata+tail_offset, tail_length);
-		data[encoding->k-1] = last_with_padding;
+		data[cur_chunk] = last_with_padding;
+	}
+	// Allocate missing padding chunks
+	for (++cur_chunk; cur_chunk < encoding->k; ++cur_chunk) {
+		data[cur_chunk] = (uint8_t*) env->calloc(encoding->block_size, sizeof(uint8_t));
+		padding_chunks++; // count padding chunks for easy deallocation
 	}
 
 	// Compute now ... damned, no return code to check
@@ -220,6 +239,10 @@ rain_encode (uint8_t *rawdata, size_t rawlength,
 	// Free allocated structures
 	if (last_with_padding)
 		env->free(last_with_padding);
+	for (; padding_chunks > 0; --padding_chunks) {
+		env->free(data[encoding->k - padding_chunks]);
+		data[encoding->k - padding_chunks] = NULL;
+	}
 	if (schedule)
 		jerasure_free_schedule(schedule);
 	if (bit_matrix)
